b953c75519f0c221d1cbd0c65bb773c54843741f
Rohit <79014667+Rohitdevops73@users.noreply.github.com>
2025-12-29 15:21:25 +0530

    update the IAC file

--- eks/main.tf
+++ eks/main.tf
@@ -2,6 +2,13 @@
 provider "aws"{
     region = "us-east-1"
 }
- resource "aws"vpc" "rohit-chatbot"{
-    
+# vpc creation
+ resource "aws"vpc" "rohitchatbot_vpc"{
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "rohitchatbot_vpc"
+    }
  }
+
+ # creating subnet
--- readme.md
+++
@@ -1,7 +0,0 @@
-# Building a chat bot and containerising the application and push the image to dockerhub
-
-![alt text](image-1.png)
-
-![alt text](image.png)
-
-![alt text](image-2.png)
